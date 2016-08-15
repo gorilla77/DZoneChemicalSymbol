3eded2d5df97f25ad8daebac8ba25b1be9ee69a4
gorilla77 <jikim77@gmail.com>
2016-08-14 23:17:58 -0700

    Add files via upload

---
+++ ChemicalSymbolService.java
@@ -0,0 +1,74 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Chemical symbol service for planet Splurth.
+ */
+public class ChemicalSymbolService {
+
+    public Boolean isValidSymbol(String elementName, String symbol) {
+        // Validate input
+        validateElementName(elementName);
+
+        // Valid symbol?
+        elementName = elementName.trim().toLowerCase();
+        symbol = symbol.trim().toLowerCase();
+        return elementName.matches("\\w*" + symbol.charAt(0) + "\\w*" + symbol.charAt(1) + "\\w*");
+    }
+
+    public String getFirstValidSymbol(String elementName) {
+        // Validate input
+        validateElementName(elementName);
+
+        // Find first char
+        elementName = elementName.trim().toLowerCase();
+        char firstChar = elementName.charAt(0);
+        int firstCharPos = 0;
+        for (int i = 1; i < elementName.length()-1; i++) {
+            char c = elementName.charAt(i);
+            if (c < firstChar) {
+                firstChar = c;
+                firstCharPos = i;
+            }
+        }
+
+        // Find second char
+        elementName = elementName.substring(firstCharPos+1);
+        char secondChar = elementName.charAt(0);
+        for (int i = 1; i < elementName.length(); i++) {
+            char c = elementName.charAt(i);
+            if (c < secondChar) {
+                secondChar = c;
+            }
+        }
+
+        return new String(new char[]{Character.toUpperCase(firstChar), secondChar});
+    }
+
+    public Set<String> getDistinctSymbols(String elementName) {
+        // Validate input
+        validateElementName(elementName);
+
+        // Find symbols
+        Set<String> symbols = new HashSet<>();
+        for (int i = 0; i < elementName.length()-1; i++) {
+            // Get first char
+            char c1 = elementName.charAt(i);
+            for (int j = i+1; j < elementName.length(); j++) {
+                // Get second char
+                char c2 = elementName.charAt(j);
+                symbols.add(new String(new char[]{Character.toUpperCase(c1), c2}));
+            }
+        }
+
+        return symbols;
+    }
+
+
+    private void validateElementName(String elementName) {
+        if (elementName == null || elementName.trim().length() == 0)
+            throw new RuntimeException("Element name is required");
+        if (elementName.trim().length() < 2)
+            throw new RuntimeException("Element name must be at least 2 characters");
+    }
+}
---
+++ ChemicalSymbolServiceTest.java
@@ -0,0 +1,42 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class ChemicalSymbolServiceTest {
+    private static ChemicalSymbolService service;
+
+    @BeforeClass
+    public static void setup() {
+        service = new ChemicalSymbolService();
+    }
+
+    @Test
+    public void isValidSymbol() throws Exception {
+        assertTrue(service.isValidSymbol("Spenglerium", "Ee"));
+        assertTrue(service.isValidSymbol("Zeddemorium", "Zr"));
+        assertTrue(service.isValidSymbol("Venkmine", "Kn"));
+        assertFalse(service.isValidSymbol("Stantzon", "Zt"));
+        assertFalse(service.isValidSymbol("Melintzum", "Nn"));
+        assertFalse(service.isValidSymbol("Tullium", "Ty"));
+    }
+
+    @Test
+    public void getFirstValidSymbol() {
+        assertEquals(service.getFirstValidSymbol("Gozerium"), "Ei");
+        assertEquals(service.getFirstValidSymbol("Slimyrine"), "Ie");
+    }
+
+    @Test
+    public void getDistinctSymbols() {
+        String elementName = "Zuulon";
+        Set<String> symbols = service.getDistinctSymbols(elementName);
+        assertEquals(symbols.size(), 11);
+        for (String symbol : symbols) {
+            assertTrue(service.isValidSymbol(elementName, symbol));
+        }
+    }
+
+}
